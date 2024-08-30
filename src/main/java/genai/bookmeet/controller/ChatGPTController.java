6f48c831b118970fa4fae67a83254cffc9005136
jihee <jiheeb@modim.co.kr>
2024-08-31 00:52:28 +0900

    1. requestChatGPT 함수 수정 2. step2, step3 초안 3. 오타 수정

--- src/main/java/genai/bookmeet/controller/ChatGPTController.java
+++ src/main/java/genai/bookmeet/controller/ChatGPTController.java
@@ -34,4 +34,26 @@ public class ChatGPTController {
 
         return ResponseEntity.ok(chatgptService.getStep1(json));
     }
+
+    @PostMapping(value = "/step2")
+    public ResponseEntity<String> ChatStep2(@RequestParam("json") String json) {
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String strCurtime = d.format(timestamp).toString();
+        System.out.println("[step2]" + strCurtime);
+
+        return ResponseEntity.ok(chatgptService.getStep2(json));
+    }
+
+    @PostMapping(value = "/step3")
+    public ResponseEntity<String> ChatStep3(@RequestParam("json") String json) {
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String strCurtime = d.format(timestamp).toString();
+        System.out.println("[step3]" + strCurtime);
+
+        return ResponseEntity.ok(chatgptService.getStep3(json));
+    }
 }
--- src/main/java/genai/bookmeet/service/ChatGPTService.java
+++ src/main/java/genai/bookmeet/service/ChatGPTService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -42,23 +43,23 @@ public class ChatGPTService {
         JSONObject jsonObject = new JSONObject(json);
         String userId = jsonObject.getString("userId");
         String text = jsonObject.getString("text");
-        String bootType = "";
+        String bookType = "";
         String character = "";
         int code = -500;
 
         String systemText = "입력된 책 제목을 확인하고, 책의 장르를 판별합니다. 장르가 문학이면 '대화할 등장인물의 이름이 무엇인가요?'라고 질문하고, 장르가 비문학이면 '대화할 저자의 이름이 무엇인가요?'라고 질문합니다. 책 제목이 이해되지 않으면 '정확히 이해하지 못했어요. 다시 말씀해 주실 수 있나요?'라고 답합니다. 이 외에 다른 정보는 출력하지 않습니다.";
         String userText = text;
-        String gptRespond = RequestChatGPT(userId, systemText, userText);
+        String gptRespond = RequestChatGPT(userId, systemText, userText, new ArrayList<>());
 
         // TODO 프롬프트 // 대화할 등장인물의 이름이 무엇인가요? 부분을 //문학 으로 변경하면 좀 더 clear 할 것 같음
         if (gptRespond.compareToIgnoreCase("대화할 등장인물의 이름이 무엇인가요?") == 0) {
-            bootType = "문학";
+            bookType = "문학";
             code = 100;
         } else if (gptRespond.compareToIgnoreCase("대화할 등장인물의 이름이 무엇인가요?") == 0) {
-            bootType = "비문학";
+            bookType = "비문학";
             code = 100;
         } else {
-            bootType = "";
+            bookType = "";
             code = -100;
         }
 
@@ -73,14 +74,14 @@ public class ChatGPTService {
         }
 
         System.out.println("book:" + text);
-        System.out.println("bootType:" + bootType);
+        System.out.println("bookType:" + bookType);
         System.out.println("character:" + character);
         System.out.println("text:" + gptRespond);
         System.out.println("code:" + code);
 
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("book", text);
-        map.put("bootType", bootType);
+        map.put("bookType", bookType);
         map.put("character", character);
         map.put("text", gptRespond);
         map.put("code", code);
@@ -90,7 +91,146 @@ public class ChatGPTService {
 
     }
 
-    private String RequestChatGPT(String userId, String systemPrompt, String userPrompt) {
+    public String getStep2(String json) {
+
+        JSONObject jsonObject = new JSONObject(json);
+        String userId = jsonObject.getString("userId");
+        String text = jsonObject.getString("text");
+        String book = jsonObject.getString("book");
+        String bookType = jsonObject.getString("bookType");
+        String character = "";
+        int code = -500;
+
+        String systemText = "";
+        if (bookType.compareTo("문학") == 0) {
+            systemText = String.format(
+                    "입력된 %s 이름을 확인하고, 그 %s이 %s에 등장하는 인물과 일치하는지 판별합니다. 맞다면 '%s과의 대화를 시작할게요' 라고 답하고, 맞지 않다면 '정확하게 이해하지 못했어요. 다시 말씀해 주실 수 있나요?' 라고 대답합니다. 이 외에 다른 정보는 출력하지 않습니다.",
+                    text, text, book, text);
+        } else { // if (bookType.compareTo("비문학") == 0) {
+            systemText = String.format(
+                    "입력된 %s 이름을 확인하고, 그 %s가 %s의 저자와 일치하는지 판별합니다. 맞다면 '%s와의 대화를 시작할게요' 라고 답하고, 맞지 않다면 '정확하게 이해하지 못했어요. 다시 말씀해 주실 수 있나요?' 라고 대답합니다. 이 외에 다른 정보는 출력하지 않습니다.",
+                    text, text, book, text);
+        }
+
+        String userText = text;
+        String gptRespond = RequestChatGPT(userId, systemText, userText, new ArrayList<>());
+
+        if (gptRespond.contains("대화를 시작할게요") == true) {
+            code = 100;
+            character = text;
+        } else {
+
+            System.out.println("[Error] chat gpt response: " + gptRespond);
+
+            code = -100;
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("book", book);
+            map.put("bookType", bookType);
+            map.put("character", character);
+            map.put("text", gptRespond);
+            map.put("code", code);
+
+            JSONObject j = new JSONObject(map);
+            return j.toString();
+        }
+
+        // 역할 부여
+        systemText = "";
+        if (bookType.compareTo("문학") == 0) {
+            systemText = String.format(
+                    "이제부터 당신은 %s의 등장인물 %s입니다. 이 역할을 부여받은 순간부터, %s의 관점에서 대답하세요. 또한 언제나 일관된 어투를 사용하세요. 만약 %s의 관점에서 대답하기 어려운 상황이라면, 이해하지 못했으니 다시 입력하라는 의미의 답변을 %s의 어투로 대답하세요. 또한 이 요청에 대한 답변 없이, %s에 맞는 자기소개를 포함해 말을 걸어주시고, 앞으로의 모든 답변은 4줄을 넘기지 마세요. 시작하세요.",
+                    book, character, character, character, character, character);
+        } else { // if (bookType.compareTo("비문학") == 0) {
+            systemText = String.format(
+                    "이제부터 당신은 %s의 저자 %s입니다. 이 역할을 부여받은 순간부터, %s의 관점에서 대답하세요. 또한 언제나 일관된 어투를 사용하세요. 만약 %s의 관점에서 대답하기 어려운 상황이라면, 이해하지 못했으니 다시 입력하라는 의미의 답변을 %s의 어투로 대답하세요. 또한 이 요청에 대한 답변 없이, %s에 맞는 자기소개를 포함해 말을 걸어주시고, 앞으로의 모든 답변은 4줄을 넘기지 마세요. 시작하세요.",
+                    book, character, character, character, character, character);
+        }
+
+        userText = "안녕하세요. 자기소개를 해주세요.";
+        gptRespond = RequestChatGPT(userId, systemText, userText, new ArrayList<>());
+
+        // TODO 대화내용 DB 저장
+
+        System.out.println("book:" + book);
+        System.out.println("bookType:" + bookType);
+        System.out.println("character:" + character);
+        System.out.println("text:" + gptRespond);
+        System.out.println("code:" + code);
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("book", book);
+        map.put("bookType", bookType);
+        map.put("character", character);
+        map.put("text", gptRespond);
+        map.put("code", code);
+
+        JSONObject j = new JSONObject(map);
+        return j.toString();
+
+    }
+
+    public String getStep3(String json) {
+
+        JSONObject jsonObject = new JSONObject(json);
+        String userId = jsonObject.getString("userId");
+        String text = jsonObject.getString("text");
+        String book = jsonObject.getString("book");
+        String bookType = jsonObject.getString("bookType");
+        String character = jsonObject.getString("character");
+        ;
+        int code = -500;
+
+        String systemText = "";
+        systemText = String.format(
+                "타인이 당신과 대화를 시작할만한 질문 3가지를 다음 형식으로 출력하세요: \r\n" + //
+                        "1) 질문\r\n" + //
+                        "2) 질문\r\n" + //
+                        "3) 질문\r\n" + //
+                        "이 외에 다른 어떤 정보도 출력하지 마세요.");
+
+        String userText = text;
+
+        // 이전 대화 step2 HISTORY 무조건 넣어야함
+        List<Map<String, String>> previousMessages = new ArrayList<>();
+        previousMessages.add(new HashMap<String, String>() {
+            {
+                put("role", "user");
+                put("content", "DB에서 가져와야함");
+            }
+        });
+        previousMessages.add(new HashMap<String, String>() {
+            {
+                put("role", "assistant");
+                put("content", "DB에서 가져와야함");
+            }
+        });
+
+        String gptRespond = RequestChatGPT(userId, systemText, userText, previousMessages);
+
+        code = 100;
+
+        // TODO 대화내용 DB 저장
+
+        System.out.println("book:" + book);
+        System.out.println("bookType:" + bookType);
+        System.out.println("character:" + character);
+        System.out.println("text:" + gptRespond);
+        System.out.println("code:" + code);
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("book", text);
+        map.put("bookType", bookType);
+        map.put("character", character);
+        map.put("text", gptRespond);
+        map.put("code", code);
+
+        JSONObject j = new JSONObject(map);
+        return j.toString();
+
+    }
+
+    private String RequestChatGPT(String userId, String systemPrompt, String userPrompt,
+            List<Map<String, String>> previousMessages) {
 
         // Request API
         HttpHeaders headers = new HttpHeaders();
@@ -101,20 +241,22 @@ public class ChatGPTService {
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("model", "gpt-3.5-turbo");
 
-        List<Map<String, String>> messages = Arrays.asList(
-                new HashMap<String, String>() {
-                    {
-                        put("role", "system");
-                        put("content",
-                                systemPrompt);
-                    }
-                },
-                new HashMap<String, String>() {
-                    {
-                        put("role", "user");
-                        put("content", userPrompt);
-                    }
-                });
+        // 이전 대화 메시지 추가
+        List<Map<String, String>> messages = new ArrayList<>(previousMessages);
+
+        // 현재 시스템 및 사용자 프롬프트 추가
+        messages.add(new HashMap<String, String>() {
+            {
+                put("role", "system");
+                put("content", systemPrompt);
+            }
+        });
+        messages.add(new HashMap<String, String>() {
+            {
+                put("role", "user");
+                put("content", userPrompt);
+            }
+        });
 
         requestBody.put("messages", messages);
 
